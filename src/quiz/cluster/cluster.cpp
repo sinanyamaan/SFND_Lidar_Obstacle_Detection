c2058a02d54ae12037a4dd78d7bdf8112eab810e
sinanyamaan <sinan.yaman@adastec.com>
2024-06-04 14:24:54 -0400

    kdtree insertion and clustering visualizations

--- src/quiz/cluster/cluster.cpp
+++ src/quiz/cluster/cluster.cpp
@@ -10,7 +10,7 @@
 // Arguments:
 // window is the region to draw box around
 // increase zoom to see more of the area
-pcl::visualization::PCLVisualizer::Ptr initScene(Box window, int zoom)
+pcl::visualization::PCLVisualizer::Ptr initScene(const Box& window, const int& zoom)
 {
 	pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer ("2D Viewer"));
 	viewer->setBackgroundColor (0, 0, 0);
@@ -18,19 +18,19 @@ pcl::visualization::PCLVisualizer::Ptr initScene(Box window, int zoom)
   	viewer->setCameraPosition(0, 0, zoom, 0, 1, 0);
   	viewer->addCoordinateSystem (1.0);
 
-  	viewer->addCube(window.x_min, window.x_max, window.y_min, window.y_max, 0, 0, 1, 1, 1, "window");
+  	viewer->addCube(window.x_min, window.x_max, window.y_min, window.y_max, 0, 0, 0, 0, 0, "window");
   	return viewer;
 }
 
-pcl::PointCloud<pcl::PointXYZ>::Ptr CreateData(std::vector<std::vector<float>> points)
+pcl::PointCloud<pcl::PointXYZ>::Ptr CreateData(const std::vector<std::vector<float>>& points)
 {
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>());
   	
-  	for(int i = 0; i < points.size(); i++)
+  	for(const auto& pt : points)
   	{
   		pcl::PointXYZ point;
-  		point.x = points[i][0];
-  		point.y = points[i][1];
+  		point.x = pt[0];
+  		point.y = pt[1];
   		point.z = 0;
 
   		cloud->points.push_back(point);
@@ -44,10 +44,10 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr CreateData(std::vector<std::vector<float>> p
 }
 
 
-void render2DTree(Node* node, pcl::visualization::PCLVisualizer::Ptr& viewer, Box window, int& iteration, uint depth=0)
+void render2DTree(const std::unique_ptr<Node> &node, pcl::visualization::PCLVisualizer::Ptr& viewer, const Box& window, int iteration=0, const uint depth=0)
 {
 
-	if(node!=NULL)
+	if(node)
 	{
 		Box upperWindow = window;
 		Box lowerWindow = window;
@@ -65,17 +65,15 @@ void render2DTree(Node* node, pcl::visualization::PCLVisualizer::Ptr& viewer, Bo
 			lowerWindow.y_max = node->point[1];
 			upperWindow.y_min = node->point[1];
 		}
-		iteration++;
-
-		render2DTree(node->left,viewer, lowerWindow, iteration, depth+1);
-		render2DTree(node->right,viewer, upperWindow, iteration, depth+1);
+		render2DTree(node->left,viewer, lowerWindow, iteration+1, depth+1);
+		render2DTree(node->right,viewer, upperWindow, iteration+2, depth+1);
 
 
 	}
 
 }
 
-std::vector<std::vector<int>> euclideanCluster(const std::vector<std::vector<float>>& points, KdTree* tree, float distanceTol)
+std::vector<std::vector<int>> euclideanCluster(const std::vector<std::vector<float>>& points, const std::unique_ptr<KdTree>& tree, float distanceTol)
 {
 
 	// TODO: Fill out this function to return list of indices for each cluster
@@ -100,17 +98,16 @@ int main ()
 	pcl::visualization::PCLVisualizer::Ptr viewer = initScene(window, 25);
 
 	// Create data
-	std::vector<std::vector<float>> points = { {-6.2,7}, {-6.3,8.4}, {-5.2,7.1}, {-5.7,6.3}, {7.2,6.1}, {8.0,5.3}, {7.2,7.1}, {0.2,-7.1}, {1.7,-6.9}, {-1.2,-7.2}, {2.2,-8.9} };
+	const std::vector<std::vector<float>> points = { {-6.2,7}, {-6.3,8.4}, {-5.2,7.1}, {-5.7,6.3}, {7.2,6.1}, {8.0,5.3}, {7.2,7.1}, {0.2,-7.1}, {1.7,-6.9}, {-1.2,-7.2}, {2.2,-8.9} };
 	//std::vector<std::vector<float>> points = { {-6.2,7}, {-6.3,8.4}, {-5.2,7.1}, {-5.7,6.3} };
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData(points);
+	const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData(points);
 
-	KdTree* tree = new KdTree;
+	const auto tree = std::make_unique<KdTree>();
   
     for (int i=0; i<points.size(); i++) 
     	tree->insert(points[i],i); 
 
-  	int it = 0;
-  	render2DTree(tree->root,viewer,window, it);
+  	render2DTree(tree->root,viewer,window);
   
   	std::cout << "Test Search" << std::endl;
   	std::vector<int> nearby = tree->search({-6,7},3.0);
@@ -123,22 +120,22 @@ int main ()
   	//
   	std::vector<std::vector<int>> clusters = euclideanCluster(points, tree, 3.0);
   	//
-  	auto endTime = std::chrono::steady_clock::now();
-  	auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+	const auto endTime = std::chrono::steady_clock::now();
+	const auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
   	std::cout << "clustering found " << clusters.size() << " and took " << elapsedTime.count() << " milliseconds" << std::endl;
 
   	// Render clusters
   	int clusterId = 0;
 	std::vector<Color> colors = {Color(1,0,0), Color(0,1,0), Color(0,0,1)};
-  	for(std::vector<int> cluster : clusters)
+  	for(const auto& cluster : clusters)
   	{
   		pcl::PointCloud<pcl::PointXYZ>::Ptr clusterCloud(new pcl::PointCloud<pcl::PointXYZ>());
-  		for(int indice: cluster)
-  			clusterCloud->points.push_back(pcl::PointXYZ(points[indice][0],points[indice][1],0));
+  		for(const auto& index: cluster)
+  			clusterCloud->points.emplace_back(points[index][0],points[index][1],0);
   		renderPointCloud(viewer, clusterCloud,"cluster"+std::to_string(clusterId),colors[clusterId%3]);
   		++clusterId;
   	}
-  	if(clusters.size()==0)
+  	if(clusters.empty())
   		renderPointCloud(viewer,cloud,"data");
 	
   	while (!viewer->wasStopped ())
--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -9,47 +9,62 @@ struct Node
 {
 	std::vector<float> point;
 	int id;
-	Node* left;
-	Node* right;
+	std::unique_ptr<Node> left;
+	std::unique_ptr<Node> right;
 
-	Node(std::vector<float> arr, int setId)
-	:	point(arr), id(setId), left(NULL), right(NULL)
-	{}
-
-	~Node()
+	Node(const std::vector<float> &arr, const int &setId) : point(arr), id(setId)
 	{
-		delete left;
-		delete right;
+		left.reset();
+		right.reset();
+	}
+
+	Node(const Node &other) : point(other.point), id(other.id)
+	{
+		left.reset(other.left.get());
+		right.reset(other.right.get());
 	}
 };
 
 struct KdTree
 {
-	Node* root;
+	std::unique_ptr<Node> root;
 
-	KdTree()
-	: root(NULL)
-	{}
+	KdTree() { root.reset(); }
 
-	~KdTree()
-	{
-		delete root;
-	}
-
-	void insert(std::vector<float> point, int id)
+	void insert(const std::vector<float> &point, const int &id)
 	{
 		// TODO: Fill in this function to insert a new point into the tree
-		// the function should create a new node and place correctly with in the root 
+		// the function should create a new node and place correctly with in the root
+		insert(root, Node(point, id), 0);
+	}
 
+	void insert(std::unique_ptr<Node> &parent, const Node & node, int depth)
+	{
+		if(!parent)
+		{
+			parent = std::make_unique<Node>(node);
+		}
+		else
+		{
+			const auto dx = depth % 2;
+			if(node.point[dx] < parent->point[dx])
+			{
+				insert(parent->left, node, ++depth);
+			}
+			else
+			{
+				insert(parent->right, node, ++depth);
+			}
+		}
 	}
 
 	// return a list of point ids in the tree that are within distance of target
-	std::vector<int> search(std::vector<float> target, float distanceTol)
+	std::vector<int> search(const std::vector<float> &target, const float &distanceTol)
 	{
 		std::vector<int> ids;
 		return ids;
 	}
-	
+
 
 };
 
